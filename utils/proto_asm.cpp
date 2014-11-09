33708c7aa29f50be1e71afb8e95991f182a8f026
Akira Moroo <retrage01@gmail.com>
2014-11-09 23:49:22 +0900

    Modify to run as a simple assembler.

--- utils/proto_asm.cpp
+++ utils/proto_asm.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <cstdint>
 
 static const int str_max = 128;
 
@@ -28,288 +29,299 @@ std::vector<std::string> parse(std::string str, char c) {
     return res;
 }
 
-uint8_t get_reg(std::string str) {
-    switch(str) {
-        case "M":
-            return 0x06;
-            break;
-        case "B":
-            return 0x00;
-            break;
-        case "C":
-            return 0x01;
-            break;
-        case "D":
-            return 0x02;
-            break;
-        case "E":
-            return 0x03;
-            break;
-        case "H":
-            return 0x04;
-            break;
-        case "L":
-            return 0x05;
-            break;
-        case "A":
-            return 0x07;
-            break;
-        case "SP":
-            return 
-        default:
-            break;
-
+std::uint16_t get_reg(std::string str) {
+    std::uint16_t res;
+    
+    if(!str.compare("M"))
+        res = 0x06;
+    else if(!str.compare("B"))
+        res = 0x00;
+    else if(!str.compare("C"))
+        res = 0x01;
+    else if(!str.compare("D"))
+        res = 0x02;
+    else if(!str.compare("E"))
+        res = 0x03;
+    else if(!str.compare("H"))
+        res = 0x04;
+    else if(!str.compare("L"))
+        res = 0x05;
+    else if(!str.compare("A"))
+        res = 0x07;
+    else if(!str.compare("SP"))
+        res = 0x06;
+    else if(!str.compare("PSW"))
+        res = 0x06
+    else if(str.length() == 1) {
+        if(std::stoi(str,nullptr, 10) < 8)
+            res = std::stoi(str,nullptr, 10);
     }
+    else if(str[str.length()-1] == 'H') {
+        str = str.erase(str.length()-1, 1);
+        if(str.length()>2) {
+            //lower
+            int len = str.length();
+            res = std::stoi(str.substr(str.length()-2,2), nullptr, 16) << 8;
+            str = str.erase(str.length()-2,2);
+            //upper
+            res = res | std::stoi(str, nullptr, 16);
+        } else {
+            res = std::stoi(str, nullptr, 16);
+        }
+    }
+
+    return res;
 }
 
-uint32_t assemble(std::vector<std::string> ops) {
-    uint32_t tmp;
-    uint8_t params[2];
-    if(ops[1])
+std::uint32_t assemble(std::vector<std::string> ops) {
+    std::uint32_t tmp = 0x00;
+    std::uint16_t params[2];
+
+    if(ops.size() >= 2)
         params[0] = get_reg(ops[1]);
     else
         params[0] = 0x06;
-    if(ops[2])
+    
+    if(ops.size() >= 3)
         params[1] = get_reg(ops[2]);
     else
         params[1] = 0x06;
 
-    switch(ops[0]) {
-        "MOV":
-            tmp = 0x40 | (params[0] << 3) | params[1];
-            break;
-        "HLT":
-            tmp = 0x76;
-            break;
-        "MVI":
-            tmp = ((0x00 | (params[0] << 3)) << 8) | params[1];
-            break;
-        "INR":
-            tmp = 0x34;
-            break;
-        "DCR":
-            tmp = 0x35;
-            break;
-        "ADD":
-            tmp = 0x80 | params[0];
-            break;
-        "ADC":
-            tmp = 0x88 | params[0];
-            break;
-        "SUB":
-            tmp = 0x90 | params[0];
-            break;
-        "SBB":
-            tmp = 0x98 | params[0];
-            break;
-        "ANA":
-            tmp = 0xa0 | params[0];
-            break;
-        "XRA":
-            tmp = 0xa8 | params[0];
-            break;
-        "ORA":
-            tmp = 0xb0 | params[0];
-            break;
-        "CMP":
-            tmp = 0xb8 | params[0];
-            break;
-        "ADI":
-            tmp = (0xc6 << 8) | params[0];
-            break;
-        "ACI":
-            tmp = (0xce << 8) | params[0];
-            break;
-        "SUI":
-            tmp = (0xd6 << 8) | params[0];
-            break;
-        "SBI":
-            tmp = (0xde << 8) | params[0];
-            break;
-        "ANI":
-            tmp = (0xe6 << 8) | params[0];
-            break;
-        "XRI":
-            tmp = (0xee << 8) | params[0];
-            break;
-        "ORI":
-            tmp = (0xf6 << 8) | params[0];
-            break;
-        "CPI":
-            tmp = (0xfe << 8) | params[0];
-            break;
-        "RLC":
-            tmp = 0x07;
-            break;
-        "RRC":
-            tmp = 0x0f;
-            break;
-        "RAL":
-            tmp = 0x17;
-            break;
-        "RAR":
-            tmp = 0x1f;
-            break;
-        "DAA":
-            tmp = 0x27;
-            break;
-        "CMA":
-            tmp = 0x2f;
-            break;
-        "STC":
-            tmp = 0x37;
-            break;
-        "CMC":
-            tmp = 0x3f;
-            break;
-        "NOP:
-            tmp = 0x00;
-            break;
-        "LXI"
-            tmp = ((0x01 | (params[0] << 4)) << 16) | params[1];
-            break;
-        "DAD":
-            tmp = 0x09 | (params[0] << 4);
-            break;
-        "STAX":
-            tmp = 0x02 | (params[0] << 4);
-            break;
-        "LDAX":
-            tmp = 0x0a | (params[0] << 4);
-            break;
-        "SHLD":
-            tmp = (0x22 << 8) | params[0];
-            break;
-        "LHLD":
-            tmp = (0x2a << 8) | params[0];
-            break;
-        "STA":
-            tmp = (0x32 << 8) | params[0];
-            break;
-        "LDA":
-            tmp = (0x3a << 8) | params[0];
-            break;
-        "INX":
-            tmp = 0x03 | (params[0] << 4);
-            break;
-        "DCX":
-            tmp = 0x0b | (params[0] << 4);
-            break;
-        "OUT":
-            tmp = (0xd3 << 8) | params[0];
-            break;
-        "IN":
-            tmp = (0xdb << 8) | params[0];
-            break;
-        "XTHL":
-            tmp = 0xe3;
-            break;
-        "XCHG":
-            tmp = 0xeb;
-            break;
-        "DI":
-            tmp = 0xf3;
-            break;
-        "EI":
-            tmp = 0xfb;
-            break;
-        "PUSH":
-            tmp = 0xc5 | (params[0] << 4);
-            break;
-        "POP":
-            tmp = 0xc1 | (params[0] << 4);
-            break;
-        "PCHL":
-            tmp = 0xe9;
-            break;
-        "SPHL":
-            tmp = 0xf9;
-            break;
-        "JMP":
-            tmp = (0xc3 << 16) | (params[1] << 8) | params[0];
-            break;
-        "JNZ":
-            tmp = (0xc2 << 16) | (params[1] << 8) | params[0];
-            break;
-        "JZ":
-            tmp = (0xca << 16) | (params[1] << 8) | params[0];
-            break;
-        "JNC":
-            tmp = (0xd2 << 16) | (params[1] << 8) | params[0];
-            break;
-        "JC":
-            tmp = (0xda << 16) | (params[1] << 8) | params[0];
-            break;
-        "JPO":
-            tmp = (0xe2 << 16) | (params[1] << 8) | params[0];
-            break;
-        "JPE":
-            tmp = (0xea << 16) | (params[1] << 8) | params[0];
-            break;
-        "JP":
-            tmp = (0xf2 << 16) | (params[1] << 8) | params[0];
-            break;
-        "JM":
-            tmp = (0xfa << 16) | (params[1] << 8) | params[0];
-            break;
-        "CALL":
-            tmp = (0xcd << 16) | (params[1] << 8) | params[0];
-            break;
-        "CNZ":
-            tmp = (0xc4 << 16) | (params[1] << 8) | params[0];
-            break;
-        "CN":
-            tmp = (0xcc << 16) | (params[1] << 8) | params[0];
-            break;
-        "CNC":
-            tmp = (0xd4 << 16) | (params[1] << 8) | params[0];
-            break;
-        "CC":
-            tmp = (0xdc << 16) | (params[1] << 8) | params[0];
-            break;
-        "CP0":
-            tmp = (0xe4 << 16) | (params[1] << 8) | params[0];
-            break;
-        "CPE":
-            tmp = (0xec << 16) | (params[1] << 8) | params[0];
-            break;
-        "CP":
-            tmp = (0xf4 << 16) | (params[1] << 8) | params[0];
-            break;
-        "CM":
-            tmp = (0xfc << 16) | (params[1] << 8) | params[0];
-            break;
-        "RET":
-            tmp = 0xc9;
-            break;
-        "RNZ":
-            tmp = 0xc0;
-            break;
-        "RZ":
-            tmp = 0xc8;
-            break;
-        "RNC":
-            tmp = 0xd0;
-            break;
-        "RC":
-            tmp = 0xd8;
-            break;
-        "RPO":
-            tmp = 0xe0;
-            break;
-        "RPE":
-            tmp = 0xe8;
-            break;
-        "RP":
-            tmp = 0xf0;
-            break;
-        "RM":
-            tmp = 0xf8;
-            break;
-        "RST":
-            tmp = 0xc7 | (params[0] << 3);
-            break;
-    }
+    if(!ops[0].compare("MOV"))
+        tmp = 0x40 | (params[0] << 3) | params[1];      
+
+    else if(!ops[0].compare("HLT"))
+        tmp = 0x76;
+
+    else if(!ops[0].compare("MVI"))
+        tmp = ((0x06 | (params[0] << 3)) << 8) | params[1];
+    
+    else if(!ops[0].compare("INR"))
+        tmp = 0x04 | (params[0] << 3);
+            
+    else if(!ops[0].compare("DCR"))
+        tmp = 0x05 | (params[0] << 3);
+            
+    else if(!ops[0].compare("ADD"))
+        tmp = 0x80 | params[0];
+            
+    else if(!ops[0].compare("ADC"))
+        tmp = 0x88 | params[0];
+            
+    else if(!ops[0].compare("SUB"))
+        tmp = 0x90 | params[0];
+            
+    else if(!ops[0].compare("SBB"))
+        tmp = 0x98 | params[0];
+            
+    else if(!ops[0].compare("ANA"))
+        tmp = 0xa0 | params[0];
+            
+    else if(!ops[0].compare("XRA"))
+        tmp = 0xa8 | params[0];
+            
+    else if(!ops[0].compare("ORA"))
+        tmp = 0xb0 | params[0];
+            
+    else if(!ops[0].compare("CMP"))
+        tmp = 0xb8 | params[0];
+            
+    else if(!ops[0].compare("ADI"))
+        tmp = (0xc6 << 8) | params[0];
+            
+    else if(!ops[0].compare("ACI"))
+        tmp = (0xce << 8) | params[0];
+            
+    else if(!ops[0].compare("SUI"))
+        tmp = (0xd6 << 8) | params[0];
+            
+    else if(!ops[0].compare("SBI"))
+        tmp = (0xde << 8) | params[0];
+            
+    else if(!ops[0].compare("ANI"))
+        tmp = (0xe6 << 8) | params[0];
+            
+    else if(!ops[0].compare("XRI"))
+        tmp = (0xee << 8) | params[0];
+            
+    else if(!ops[0].compare("ORI"))
+        tmp = (0xf6 << 8) | params[0];
+            
+    else if(!ops[0].compare("CPI"))
+        tmp = (0xfe << 8) | params[0];
+            
+    else if(!ops[0].compare("RLC"))
+        tmp = 0x07;
+            
+    else if(!ops[0].compare("RRC"))
+        tmp = 0x0f;
+            
+    else if(!ops[0].compare("RAL"))
+        tmp = 0x17;
+            
+    else if(!ops[0].compare("RAR"))
+        tmp = 0x1f;
+            
+    else if(!ops[0].compare("DAA"))
+        tmp = 0x27;
+            
+    else if(!ops[0].compare("CMA"))
+        tmp = 0x2f;
+            
+    else if(!ops[0].compare("STC"))
+        tmp = 0x37;
+            
+    else if(!ops[0].compare("CMC"))
+        tmp = 0x3f;
+            
+    else if(!ops[0].compare("NOP"))
+        tmp = 0x00;
+            
+    else if(!ops[0].compare("LXI"))
+        tmp = ((0x01 | (params[0] << 4)) << 16) | params[1];
+            
+    else if(!ops[0].compare("DAD"))
+        tmp = 0x09 | (params[0] << 4);
+            
+    else if(!ops[0].compare("STAX"))
+        tmp = 0x02 | (params[0] << 4);
+            
+    else if(!ops[0].compare("LDAX"))
+        tmp = 0x0a | (params[0] << 4);
+            
+    else if(!ops[0].compare("SHLD"))
+        tmp = (0x22 << 16) | params[0];
+            
+    else if(!ops[0].compare("LHLD"))
+        tmp = (0x2a << 16) | params[0];
+            
+    else if(!ops[0].compare("STA"))
+        tmp = (0x32 << 16) | params[0];
+            
+    else if(!ops[0].compare("LDA"))
+        tmp = (0x3a << 16) | params[0];
+            
+    else if(!ops[0].compare("INX"))
+        tmp = 0x03 | (params[0] << 4);
+            
+    else if(!ops[0].compare("DCX"))
+        tmp = 0x0b | (params[0] << 4);
+            
+    else if(!ops[0].compare("OUT"))
+        tmp = (0xd3 << 8) | params[0];
+            
+    else if(!ops[0].compare("IN"))
+        tmp = (0xdb << 8) | params[0];
+            
+    else if(!ops[0].compare("XTHL"))
+        tmp = 0xe3;
+            
+    else if(!ops[0].compare("XCHG"))
+        tmp = 0xeb;
+            
+    else if(!ops[0].compare("DI"))
+        tmp = 0xf3;
+            
+    else if(!ops[0].compare("EI"))
+        tmp = 0xfb;
+            
+    else if(!ops[0].compare("PUSH"))
+        tmp = 0xc5 | (params[0] << 3);
+            
+    else if(!ops[0].compare("POP"))
+        tmp = 0xc1 | (params[0] << 3);
+            
+    else if(!ops[0].compare("PCHL"))
+        tmp = 0xe9;
+            
+    else if(!ops[0].compare("SPHL"))
+        tmp = 0xf9;
+            
+    else if(!ops[0].compare("JMP"))
+        tmp = (0xc3 << 16) | params[0];
+            
+    else if(!ops[0].compare("JNZ"))
+        tmp = (0xc2 << 16) | params[0];
+            
+    else if(!ops[0].compare("JZ"))
+        tmp = (0xca << 16) | params[0];
+            
+    else if(!ops[0].compare("JNC"))
+        tmp = (0xd2 << 16) | params[0];
+            
+    else if(!ops[0].compare("JC"))
+        tmp = (0xda << 16) | params[0];
+            
+    else if(!ops[0].compare("JPO"))
+        tmp = (0xe2 << 16) | params[0];
+            
+    else if(!ops[0].compare("JPE"))
+        tmp = (0xea << 16) | params[0];
+            
+    else if(!ops[0].compare("JP"))
+        tmp = (0xf2 << 16) | params[0];
+            
+    else if(!ops[0].compare("JM"))
+        tmp = (0xfa << 16) | params[0];
+            
+    else if(!ops[0].compare("CALL"))
+        tmp = (0xcd << 16) | params[0];
+            
+    else if(!ops[0].compare("CNZ"))
+        tmp = (0xc4 << 16) | params[0];
+            
+    else if(!ops[0].compare("CN"))
+        tmp = (0xcc << 16) | params[0];
+            
+    else if(!ops[0].compare("CNC"))
+        tmp = (0xd4 << 16) | params[0];
+            
+    else if(!ops[0].compare("CC"))
+        tmp = (0xdc << 16) | params[0];
+            
+    else if(!ops[0].compare("CP0"))
+        tmp = (0xe4 << 16) | params[0];
+            
+    else if(!ops[0].compare("CPE"))
+        tmp = (0xec << 16) | params[0];
+            
+    else if(!ops[0].compare("CP"))
+        tmp = (0xf4 << 16) | params[0];
+            
+    else if(!ops[0].compare("CM"))
+        tmp = (0xfc << 16) | params[0];
+            
+    else if(!ops[0].compare("RET"))
+        tmp = 0xc9;
+            
+    else if(!ops[0].compare("RNZ"))
+        tmp = 0xc0;
+            
+    else if(!ops[0].compare("RZ"))
+        tmp = 0xc8;
+            
+    else if(!ops[0].compare("RNC"))
+        tmp = 0xd0;
+            
+    else if(!ops[0].compare("RC"))
+        tmp = 0xd8;
+            
+    else if(!ops[0].compare("RPO"))
+        tmp = 0xe0;
+            
+    else if(!ops[0].compare("RPE"))
+        tmp = 0xe8;
+            
+    else if(!ops[0].compare("RP"))
+        tmp = 0xf0;
+            
+    else if(!ops[0].compare("RM"))
+        tmp = 0xf8;
+            
+    else if(!ops[0].compare("RST"))
+        tmp = 0xc7 | (params[0] << 3);
+
+    return tmp;
 }
 
 int main(int argc, char *argv[]) {
@@ -323,13 +335,11 @@ int main(int argc, char *argv[]) {
     
     std::string str;
 
-    int i = 0;
+    //std::vector<std::string> res;
     while(ifs.good()) {
-        std::getline(ifs, s);
-
-        }
-
-        i++;
+        std::getline(ifs, str);
+        if(str.size())
+            std::cout << std::hex << assemble(parse(str, ' ')) << std::endl;
     }
 
     return 0;
