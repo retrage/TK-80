99a5d7bfa39d94b7ac8d36c9c7f89f139899cce1
retrage <retrage01@gmail.com>
2015-04-01 02:32:23 +0900

    Delete comment out

--- tk80_cpp/memory.cpp
+++ tk80_cpp/memory.cpp
@@ -27,24 +27,6 @@ void tk80_mem::write(uint16_t addr, uint8_t data) {
 #endif
 
 }
-/*
-bool tk80_mem::load(std::string fileName) {
-    std::ifstream ifs;
-    ifs.open(fileName, std::ifstream::in|std::ifstream::binary);
-    if (!ifs) {
-        printf("Could not open the file.\n");
-        return false;
-    }
-    char c;
-    int i = 0;
-    while ((c=ifs.get())!=EOF) {
-        write(addr+i, (uint8_t)c);
-        i++;
-    }
-    ifs.close();
-    return true;
-}
-*/
 
 bool tk80_mem::load(std::ifstream ifs) {
     
